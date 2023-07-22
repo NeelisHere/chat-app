c028c592de643c2d356694342bde4aa3fa9adf25
Neelabh Paul <npaul_be19@thapar.edu>
2023-07-22 14:23:32 +0530

    name of the chat-routes modified

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -14,9 +14,9 @@ const router = express.Router()
 
 router.post('/access-chat', init_route, protect, accessChat)
 router.get('/get-chats', init_route, protect, getChats)
-router.post('/create-group', init_route, protect, createGroupChat)
-router.put('/rename-group', init_route, protect, renameGroup)
-router.put('/add-to-group', init_route, protect, addToGroup)
-router.put('/remove-from-group', init_route, protect, removeFromGroup)
+router.post('/group/create', init_route, protect, createGroupChat)
+router.put('/group/rename', init_route, protect, renameGroup)
+router.put('/group/add-user', init_route, protect, addToGroup)
+router.put('/group/remove-user', init_route, protect, removeFromGroup)
 
 module.exports = router
